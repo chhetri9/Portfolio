0019535bb966ef4b9fc6ec103649a442695fb390
chhetri9 <chhetripriyankar69@gmail.com>
2025-07-30 19:02:00 +0530

    Updated Files

--- src/Components/Projects/Projectdemo.tsx
+++ src/Components/Projects/Projectdemo.tsx
@@ -12,10 +12,11 @@ const Projectdemo = ({ title, main }: ProjectDemoProps) => {
       <p className="px-4 text-sm md:text-md leading-tight py-2">{main}</p>
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
-          Demo
+          <a target="_blank" href="https://todo-list-priyankar.vercel.app/">
+            Demo</a>
         </button>
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
-          Source Code
+          <a target="_blank" href="https://github.com/chhetri9/TODO-APP/tree/master/todo-list">Source Code</a>
         </button>
       </div>
     </div>
